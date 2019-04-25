47d9fdd3dd1c738e324e6e3e4866c59f6d31f4fb
Sergey <slic@live.ru>
2019-04-26 04:42:23 +0700

    Сверстал главное окно

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
+
 $(document).ready(function () {
+
+  /* Плавная прокрутка до якоря */
+  $(".go_to").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
 
   /* Плавно прокручиваем страницу вверх*/
   $('#upTop').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 500);
+
+    $('.navbar').toggleClass('collapsed');
     return false;
   });
 
@@ -20,6 +33,9 @@ $(document).ready(function () {
     }
   });
 
+  /* Инициализация WOW.js */
+  new WOW().init();
+
   /* Скрипт для мобильного меню */
   $('.toogle-button').click(function () {
     $('.navbar').toggleClass('collapsed');
@@ -28,12 +44,123 @@ $(document).ready(function () {
     $('.navbar').toggleClass('collapsed');
   });
 
+  /* Маска для телефона*/
+  $('.phone').mask('+7 (999) 999-99-99');
+
+  /* Валидация форм */
+  var validator1 = $('#request-form').validate({
+    rules: {
+      user_name: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      user_phone: {
+        required: true
+      },
+      user_email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      user_name: {
+        required: "Заполните поле",
+        minlength: "Поле должно содержать минимум 2 символа",
+        maxlength: "Поле не должно превышать 15 символов"
+      },
+      user_phone: "Заполните поле",
+      user_email: {
+        required: "Заполните поле",
+        email: "Введите корректный email"
+      }
+    },
+    errorClass: "invalid",
+    errorElement: "div"
+  });
+
+  var validator2 = $('#modal-form').validate({
+    rules: {
+      user_name: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      user_phone: {
+        required: true
+      },
+      user_email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      user_name: {
+        required: "Заполните поле",
+        minlength: "Поле должно содержать минимум 2 символа",
+        maxlength: "Поле не должно превышать 15 символов"
+      },
+      user_phone: "Заполните поле",
+      user_email: {
+        required: "Заполните поле",
+        email: "Введите корректный email"
+      }
+    },
+    errorClass: "invalid",
+    errorElement: "div"
+  });
+
+  /* Инициализация слайдера */
+  $('.projects .slider__items').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: "<img class='prev slick-prev' src='img/left-arrow.svg'>",
+    nextArrow: "<img class='next slick-next' src='img/right-arrow.svg'>"
+  });
+
+  $('.clients .slider__items').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+    prevArrow: "<img class='prev slick-prev' src='img/left-arrow.svg'>",
+    nextArrow: "<img class='next slick-next' src='img/right-arrow.svg'>"
+  });
+
   /* Модальное окно */
   var modal = $('#modal');
   var closeModalButton = $('#close');
+  var callme = $('.call__dialog');
   var actionButton = $('.modal__button');
 
   actionButton.on('click', function () {
+
+    callme.removeClass("animated").addClass("animated");
+
     var modalType = $(this).data('modal-type');
 
     switch (modalType) {
@@ -58,85 +185,13 @@ $(document).ready(function () {
     modal.addClass('modal_active');
   });
 
-  closeModalButton.on('click', function () {
+    closeModalButton.on('click', function () {
     modal.removeClass('modal_active');
+
+      $('#modal').find('.modal__form')[0].reset();
   });
-
-  /* Маска для телефона*/
-  $('.phone').mask('+7 (999) 999-99-99');
-
-  /* Валидация форм */
-  $('#request-form').validate({
-
-    rules: {
-      user_name: {
-        required: true,
-        minlength: 2,
-        maxlength: 15
-      },
-      user_phone: {
-        required: true
-      },
-      user_email: {
-        required: true,
-        email: true
-      }
-    },
-    messages: {
-      user_name: {
-        required: "Заполните поле",
-        minlength: "Поле должно содержать минимум 2 символа",
-        maxlength: "Поле не должно превышать 15 символов"
-      },
-      user_phone: "Заполните поле",
-      user_email: {
-        required: "Заполните поле",
-        email: "Введите корректный email"
-      }
-    },
-    errorClass: "invalid",
-    errorElement: "div"
-  });
-
-  $('#modal-form').validate({
-
-    rules: {
-      user_name: {
-        required: true,
-        minlength: 2,
-        maxlength: 15
-      },
-      user_phone: {
-        required: true
-      },
-      user_email: {
-        required: true,
-        email: true
-      }
-    },
-    messages: {
-      user_name: {
-        required: "Заполните поле",
-        minlength: "Поле должно содержать минимум 2 символа",
-        maxlength: "Поле не должно превышать 15 символов"
-      },
-      user_phone: "Заполните поле",
-      user_email: {
-        required: "Заполните поле",
-        email: "Введите корректный email"
-      }
-    },
-    errorClass: "invalid",
-    errorElement: "div"
-  });
-
-
- 
 
 });
-
-
-
 
 /* Инициаизация yandex карты */
 ymaps.ready(init);
